be8552da43b99281c32761c2ee657f5a2182532e
Rorosam24 <111056571+Rorosam24@users.noreply.github.com>
2022-11-16 21:40:20 +0200

    Update cd.c

--- cd.c
+++ cd.c
@@ -75,7 +75,7 @@ void cd_to(data_shell *datash)
 	cp_pwd = _strdup(pwd);
 	set_env("OLDPWD", cp_pwd, datash);
 
-	cp_dir = _strdup(dir);
+	cp_dir =_strdup(dir);
 	set_env("PWD", cp_dir, datash);
 
 	free(cp_pwd);
